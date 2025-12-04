fb38d844ffb5789ab8b2707dda541af51f69c5ef
thabomosenthal <thabo.mosenthal@hotmail.com>
2025-12-04 21:59:22 +0200

    feat(analytics): add Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import GameProvider from "@/components/game-provider";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<GameProvider>{children}</GameProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
